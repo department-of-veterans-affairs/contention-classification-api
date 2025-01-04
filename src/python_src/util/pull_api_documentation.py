b6d2b3ccbea07257f6f1336478636fcc41a4fcd6
Gabriel Zurita <gabezurita@gmail.com>
2025-01-03 18:01:18 -0700

    docs: update API documentation script for Poetry
    - Update usage instructions to use Poetry instead of direct Python
    - Remove outdated Python 3.10 reference
    - Remove PYTHONPATH setup requirement
    - Add clearer output file documentation
    - Remove outdated path references
    
    This change aligns the documentation script with our Poetry-based
    workflow and Python 3.12.3 environment.

--- src/python_src/util/pull_api_documentation.py
+++ src/python_src/util/pull_api_documentation.py
@@ -2,8 +2,10 @@
 This standalone script pulls the OpenAPI specification of the FastAPI app and exports it to a JSON file.
 This can be used to move the Swagger documentation to other tools, like a java project.
 
-Make sure you are in cc-app/src/ and have set your PYTHONPATH accordingly in order to execute
-python3.10 pull_api_documentation.py
+Usage:
+    poetry run python src/python_src/util/pull_api_documentation.py
+
+The script will generate a fastapi.json file containing the OpenAPI specification.
 """
 
 import json
@@ -14,16 +16,14 @@ from fastapi.openapi.utils import get_openapi
 
 def export_openapi(app: FastAPI, filename: str) -> None:
     """Export the OpenAPI specification of a FastAPI app to a JSON file."""
-    openapi_schema = get_openapi(
-        title=app.title, version=app.version, routes=app.routes
-    )
+    openapi_schema = get_openapi(title=app.title, version=app.version, routes=app.routes)
 
-    with open(filename, "w") as outfile:
+    with open(filename, 'w') as outfile:
         json.dump(openapi_schema, outfile, indent=4)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from api import app
 
-    export_openapi(app, "fastapi.json")
-    print("Done!")
+    export_openapi(app, 'fastapi.json')
+    print('Done!')
