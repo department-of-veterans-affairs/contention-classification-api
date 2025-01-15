dd5dd7b41fce91760b77fcef4e39f24587a5388e
broskj <170366505+brostk@users.noreply.github.com>
2025-01-15 10:40:37 -0500

    correct argument and return types

--- src/python_src/util/brd_classification_codes.py
+++ src/python_src/util/brd_classification_codes.py
@@ -7,17 +7,17 @@ from typing import Dict, Optional
 BRD_CLASSIFICATIONS_PATH = os.path.join(os.path.dirname(__file__), "data", "lh_brd_classification_ids.json")
 
 
-def get_classification_names_by_code() -> Dict[str, str]:
+def get_classification_names_by_code() -> Dict[int, str]:
     name_by_code = {}
     with open(BRD_CLASSIFICATIONS_PATH, "r") as fh:
         disability_items = json.load(fh)["items"]
         for item in disability_items:
-            name_by_code[str(item["id"])] = item["name"]
+            name_by_code[item["id"]] = item["name"]
     return name_by_code
 
 
 CLASSIFICATION_NAMES_BY_CODE = get_classification_names_by_code()
 
 
-def get_classification_name(classification_code: str) -> Optional[str]:
+def get_classification_name(classification_code: int) -> Optional[str]:
     return CLASSIFICATION_NAMES_BY_CODE.get(classification_code)
--- tests/test_brd_classification_codes.py
+++ tests/test_brd_classification_codes.py
@@ -26,9 +26,9 @@ def test_first_function(test_client: TestClient) -> None:
     with patch("builtins.open", mock_open(read_data=json.dumps(mock_data))):
         result = get_classification_names_by_code()
         assert result == {
-            "8989": "Mental Disorders",
-            "8997": "Musculoskeletal - Knee",
-            "3140": "Hearing Loss",
+            8989: "Mental Disorders",
+            8997: "Musculoskeletal - Knee",
+            3140: "Hearing Loss",
         }
 
 
@@ -57,10 +57,10 @@ def test_fourth_function(test_client: TestClient) -> None:
         ]
     }
     with patch("builtins.open", mock_open(read_data=json.dumps(mock_data))):
-        assert get_classification_name("8989") == "Mental Disorders"
-        assert get_classification_name("8997") == "Musculoskeletal - Knee"
-        assert get_classification_name("3140") == "Hearing Loss"
-        assert get_classification_name("9999") is None
+        assert get_classification_name(8989) == "Mental Disorders"
+        assert get_classification_name(8997) == "Musculoskeletal - Knee"
+        assert get_classification_name(3140) == "Hearing Loss"
+        assert get_classification_name(9999) is None
 
 
 def test_fifth_function(test_client: TestClient) -> None:
@@ -73,5 +73,5 @@ def test_fifth_function(test_client: TestClient) -> None:
 def test_sixth_function(test_client: TestClient) -> None:
     """Test error handling when file cannot be opened."""
     with patch.dict(CLASSIFICATION_NAMES_BY_CODE, {}, clear=True):
-        result = get_classification_name("8989")
+        result = get_classification_name(8989)
         assert result is None
