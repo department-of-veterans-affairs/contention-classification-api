3ab8f39947d4d08d91055d8aaedb13d919d27e40
Lisa Chung <lisac@users.noreply.github.com>
2025-06-17 16:07:43 -0400

    add script for running input files through classifiers (#106)
    * initial revision: a file of sample inputs, and a script that reads in that file and creates outputs
    
    * update to use scikit-learn instead of attempting to compute scores ourselves
    
    * update to use scikit-learn instead of attempting to compute scores ourselves
    
    * to better illustrate the script's actions: create a smaller input file and mock 'classifiers'
    
    * fix lint
    
    * refine comment on how to run the script
    
    * update comment
    
    * Update poetry.lock using Poetry 2.0.0
    
    * fix(?) python type hints
    
    * fix formatting
    
    * fix issue detected by the bandit CI check
    ([B110:try_except_pass] Try, Except, Pass detected.)
    
    * upload sample output files
    
    * simplify (slightly) reading in of the input file
    push the tokenizing to later in the code. i think this makes the script a little more readable.
    
    * refactoring
    separate the logic of running the classifier and compiling the text for the output file
    
    * leverage sklearn instead of building the scores file ourselves
    
    * add logic to output file of aggregate predictions
    
    * remove one of the demo classifiers
    why: it served its purposes in helping me understand the code.
    
    * use classes instead of layers and layers of tuples and lists
    
    * move classes to a separate file
    
    * move script documentation to a README
    
    * update README with a sample of the scores output
    
    * fix README format
    
    * update sample output files
    
    * use classes instead of tuples and lists, cont'd
    (this should have been included as part of 2a314cfc3)
    
    * minor edits for readability
    
    * use csv library to read and write files, and minor update to column names
    why use csv library: more readable, which has advantages for maintaining the code
    
    * fixes for format/lint
    
    * fix variable name
    
    * fix mismatch between code and comment
    
    * update logic around writing the scores/metrics file
    use the more efficient write() than writelines(), and minor renames to avoid re-using  as a variable name, to avoid confusion with the classification_report() imported function
    
    * update logic around writing the scores/metrics file
    use the more efficient write() than writelines(), and minor renames to avoid re-using 'classification_report' as a variable name, to avoid confusion with the classification_report() imported function
    
    * add unit tests
    
    * fixes for format/lint
    
    * fix method name to be more descriptive
    
    * fix format
    
    * update sample output files
    
    ---------
    
    Co-authored-by: Lisa Chung <943046+lisac@users.noreply.github.com>
    Co-authored-by: abd-vro-machine <118635145+abd-vro-machine@users.noreply.github.com>

---
+++ src/python_src/util/data/simulations/classifiers.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from python_src.util.app_utilities import expanded_lookup_table
+
+
+class BaseClassifierForSimulation:
+    name: str
+    predictions: List[str]
+
+    def make_predictions(self, conditions: List[str]) -> None:
+        pass
+
+
+class ProductionClassifier(BaseClassifierForSimulation):
+    name = "csv_lookup"
+
+    def make_predictions(self, conditions: List[str]) -> None:
+        predicted_classifications: List[str] = []
+        for condition in conditions:
+            try:
+                prediction = str(expanded_lookup_table.get(condition).get("classification_code"))
+            except Exception as e:
+                print("error: ", e)
+
+            predicted_classifications.append(prediction)
+        self.predictions = predicted_classifications
+
+
+class RespiratoryClassifier(BaseClassifierForSimulation):
+    """For demo purposes: a classifier that always predicts a classification
+    of respiratory, which maps to classification code '9012' """
+
+    name = "respiratory_classifier"
+
+    def make_predictions(self, conditions: List[str]) -> None:
+        self.predictions = ["9012"] * len(conditions)
