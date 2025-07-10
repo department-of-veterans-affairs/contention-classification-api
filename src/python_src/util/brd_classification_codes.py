2013e28b9d74d7ff0a98e43a430be9aa73d0a5d5
= <=>
2025-07-10 14:02:42 -0500

    working

--- src/python_src/util/brd_classification_codes.py
+++ src/python_src/util/brd_classification_codes.py
@@ -4,6 +4,8 @@ from typing import Dict, Optional
 
 from .app_utilities import dc_lookup_table
 
+import datetime
+
 # sourced from Lighthouse Benefits Reference Data /disabilities endpoint:
 # https://developer.va.gov/explore/benefits/docs/benefits_reference_data?version=current
 BRD_CLASSIFICATIONS_PATH = os.path.join(os.path.dirname(__file__), "data", "lh_brd_classification_ids.json")
@@ -43,16 +45,25 @@ CLASSIFICATION_CODES_BY_ID = get_classification_by_code()
 
 
 def get_classification(classification_code: int = None, classification_name: str = None) -> Optional[dict]:
+    print("\n *** \n")
     print(f"\nclassification_code: {classification_code}")
-    print(f"classification_name: {classification_name}")
-    print(CLASSIFICATION_CODES_BY_ID)
-    print(get_classification_by_code())
+    # print(f"classification_name: {classification_name}")
+    # print(CLASSIFICATION_CODES_BY_ID)
+    # print(get_classification_by_code())
+    # print(classification.get('endDateTime'))
     if classification_code:
         # classification = dc_lookup_table.get(str(classification_code))
         # classification.update(CLASSIFICATION_CODES_BY_ID.get(classification_code))
-        classification = CLASSIFICATION_CODES_BY_ID.get(classification_code)
+        # classification = CLASSIFICATION_CODES_BY_ID.get(classification_code)
+        classification = get_classification_by_code().get(str(classification_code))
     elif classification_name: 
         # classification = dc_lookup_table.get(classification_name)
         # classification.update(CLASSIFICATION_CODES_BY_ID.get(classification_name))
-        classification = CLASSIFICATION_CODES_BY_ID.get(classification_name)
+        classification = get_classification_by_code().get(str(classification_name))
+    print(f"classification: {classification}")
+    if  "endDateTime" in classification.keys() and \
+            classification.get("endDateTime") is not None and \
+            datetime.datetime.strptime(classification.get("endDateTime"), "%Y-%m-%dT%H:%M:%SZ") < datetime.datetime.now():
+        classification['id'] = None
+        classification['name'] = None 
     return classification
