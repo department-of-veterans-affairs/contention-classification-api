aea36976ef34b02eb27124094c56b258e31dabbc
= <=>
2025-07-28 08:58:55 -0500

    Fix mypy type errors in test_ml_classifier.py

--- tests/test_ml_classifier.py
+++ tests/test_ml_classifier.py
@@ -1,10 +1,12 @@
 import logging
 import os
 import string
+from typing import Any
 from unittest.mock import MagicMock, call, patch
 
 import boto3
 import pytest
+from moto import mock_aws
 from numpy import float32, ndarray
 from onnx.helper import make_node
 from scipy.sparse import csr_matrix
@@ -151,3 +153,45 @@ def test_s3() -> None:
 def test_invoke_mlClassifier() -> None:
     classifier = MLClassifier()
     classifier.download_models_from_s3()
+
+
+BUCKET_NAME = "mybucket"
+FILE_NAME = "README.md"
+FILE_LOCATION = FILE_NAME
+
+
+@mock_aws  # type: ignore[misc]
+def test_client() -> None:
+    create_bucket()
+    s3 = boto3.client("s3", region_name="us-east-1")
+
+    with open(FILE_LOCATION, "rb") as data:
+        s3.upload_fileobj(data, BUCKET_NAME, FILE_NAME)
+    verify_upload()
+
+
+@mock_aws  # type: ignore[misc]
+def test_resource() -> None:
+    s3_resource, _ = create_bucket()
+    s3_resource.meta.client.upload_file(FILE_LOCATION, BUCKET_NAME, FILE_NAME)
+    verify_upload()
+
+
+@mock_aws  # type: ignore[misc]
+def test_bucket_resource() -> None:
+    _, bucket = create_bucket()
+    bucket.upload_file(FILE_LOCATION, FILE_NAME)
+    verify_upload()
+
+
+def verify_upload() -> None:
+    client = boto3.client("s3", region_name="us-east-1")
+    resp = client.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
+    content_length = resp["ResponseMetadata"]["HTTPHeaders"]["content-length"]
+    print("Content-Length: {}".format(content_length))
+
+
+def create_bucket() -> tuple[Any, Any]:
+    s3 = boto3.resource("s3", region_name="us-east-1")
+    bucket = s3.create_bucket(Bucket=BUCKET_NAME)
+    return s3, bucket
